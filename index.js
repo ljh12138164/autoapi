03261280398dfc0c0349d48e2f010f370c6ffc37
一秋 <1464074871@qq.com>
2025-05-26 16:20:13 +0800

    feat:初始化仓库

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+})
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+})
