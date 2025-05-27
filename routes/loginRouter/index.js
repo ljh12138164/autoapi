70843cad1c68c7affad548d29fd38a57ffacceab
一秋 <1464074871@qq.com>
2025-05-27 11:58:53 +0800

    feat:封装knex

--- routes/loginRouter/index.js
+++ routes/loginRouter/index.js
@@ -1,24 +1,5 @@
 import express from 'express';
-import knex from 'knex';
-const db = knex({
-    client: 'mysql',
-    connection: {
-        host: '127.0.0.1',
-        port: 3306,
-        user: 'root',
-        password: 'root',
-        database: 'auto_form'
-    },
-    pool: {
-        min: 2,       // 最小空闲连接数
-        max: 10,      // 最大连接数
-        idleTimeoutMillis: 30000, // 连接空闲超时时间（毫秒）
-        acquireTimeoutMillis: 60000, // 获取连接超时时间（毫秒）
-        createTimeoutMillis: 30000, // 创建连接超时时间（毫秒）
-        destroyTimeoutMillis: 5000, // 销毁连接超时时间（毫秒）
-        propagateCreateError: false // 是否传播创建连接时的错误
-    }
-})
+import db from "../../config/db.js"
 db.schema.createTableIfNotExists('users', (table) => {
     table.increments('id').primary();
     table.string('username');
